7bf6a929506c87efcc42d6b2bbeb00e5844e951c
amcarro3 <amcarro3@asu.edu>
2021-10-12 20:05:43 -0700

    Add basic text panel
    Start research into buttons

---
+++ Assets/Assembly/Code/ButtonFunctions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void HideText()
+    {
+        Debug.Log("Hide the text");
+    }
+}
